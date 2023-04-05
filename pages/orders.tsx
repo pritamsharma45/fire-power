4c3cddf3dd2d697f48e6379e3038c29621044fad
pritamsharma45 <notifications.borl@gmail.com>
2023-04-05 14:40:18 +0530

    Bug fixing

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -21,7 +21,6 @@ const GET_ORDERS = gql`
 `;
 
 export default function AllOrders() {
-  
   const { data: session, status } = useSession();
   console.log("Session", session);
 
@@ -36,13 +35,16 @@ export default function AllOrders() {
   });
   console.log(orders);
 
-
-
-
   return (
     <div className="container mx-auto max-w-5xl my-5 px-5">
       <h1 className="text-xl font-bold mt-2">Orders</h1>
-      {orders?.orderItems && <Orders orders={orders?.orderItems} />}
+      {orders?.orderItems ? (
+        <Orders orders={orders?.orderItems} />
+      ) : (
+        <div className="text-center">
+          <h1 className="text-xl font-bold mt-2">No orders to display!</h1>
+        </div>
+      )}
     </div>
   );
 }
