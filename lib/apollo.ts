c5abf4e22f8f86a63df7eb765c48a65502e0bafc
pritamsharma45 <notifications.borl@gmail.com>
2023-05-10 13:19:05 +0530

    url changed

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 const devUrl = "http://localhost:3000/api/graphql";
-const prodUrl = "https://nextjs-ecommerce-ten-delta.vercel.app/api/graphql";
+const prodUrl = "https://nextjs-ecommerce-ashy.vercel.app/api/graphql";
 // const prodUrl = `https://${process.env.VERCEL_URL}/api/graphql`;
 const url = process.env.NODE_ENV === "production" ? prodUrl : devUrl;
 
