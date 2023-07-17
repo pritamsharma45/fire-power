2dbab9d4185182713b5ecf8400e913832af74bef
pritamsharma45 <notifications.borl@gmail.com>
2023-07-17 13:34:27 +0530

    Sdfads

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,8 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 const devUrl = "http://localhost:3000/api/graphql";
-const prodUrl =
-  "https://nextjs-ecommerce-pritamsharma45.vercel.app/api/graphql";
+const prodUrl = "https://woodhood.co.in/api/graphql";
 // const prodUrl = `https://${process.env.VERCEL_URL}/api/graphql`;
 const url = process.env.NODE_ENV === "production" ? prodUrl : devUrl;
 
